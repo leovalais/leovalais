d6128f3a99e3abe05f14b9fc6c792f191390ee2c
Leo Valais <leo.valais97@gmail.com>
2019-07-20 02:21:00 +0200

    initial commit

---
+++ src/leovalais/core.cljs
@@ -0,0 +1,18 @@
+(ns leovalais.core
+    (:require
+      [reagent.core :as r]))
+
+;; -------------------------
+;; Views
+
+(defn home-page []
+  [:div [:h2 "Welcome to Léo Valais' home-page"]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (r/render [home-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (mount-root))
